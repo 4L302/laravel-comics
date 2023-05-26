4909e9585c4c0f757a2ba0b9ffb3cb491d7ba877
4L302 <alegarg2002@gmail.com>
2023-05-26 16:07:34 +0200

    vite e bootstap

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,15 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel([
-            'resources/css/app.css',
+            'resources/scss/app.scss',
             'resources/js/app.js',
         ]),
     ],
+    /* Aggiungere un alias per la certella resources */
+    resolve:{
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            'resources': '/resources/'
+        }
+    }
 });
